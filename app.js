844b907803ffe3bbb93aa641aecb77d33ee7252f
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-05 20:09:23 +0600

    fixed slider image not showing

--- app.js
+++ app.js
@@ -68,6 +68,9 @@ const createSlider = () => {
   // hide image aria
   imagesArea.style.display = 'none';
   const duration = document.getElementById('duration').value || 1000;
+  if(duration < 0){
+    return alert('Please Input a positive Duration')
+  }
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
